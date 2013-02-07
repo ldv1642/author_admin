94aed0baac7486fa5f8d76514df29d3250891bb6
ldv1642 <ldv1642@live.com>
2013-02-07 10:27:12 +0000

    init

---
+++ AuthorsDB/DBAccess.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Web.UI.WebControls;
+
+
+namespace AuthorsDB
+{
+    public class DBAccess
+    {
+        static string connString = WebConfigurationManager.ConnectionStrings["db"].ConnectionString;
+
+        public static List<Author> GetAuthors()
+        {
+            return null;
+        }
+
+        public static Author GetAuthor(string id)
+        {
+            return null;
+        }
+
+        public static int UpdateAuthor(Author author)
+        {
+            return 0;
+        }
+
+        public static int InsertAuthor(Author author)
+        {
+            return 0;
+        }
+
+        public static bool CheckDB()
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                if (!string.IsNullOrEmpty(conn.ServerVersion))
+                {
+                    conn.Close();
+                    return true;
+                }
+                else
+                {
+                    conn.Close();
+                    return false;
+                }
+            }
+        }
+    }
+
+    public class Author
+    {
+
+    }
+}
---
+++ Test/TestDB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using AuthorsDB;
+
+namespace Test
+{
+    [TestFixture]
+    public class TestDB
+    {
+        //access db - done
+        //get all authors
+        //get an author
+        //update an author
+        //delete an author
+        //insert an author
+
+        [TestCase]
+        public void CanConnectToDB()
+        {
+            bool available = DBAccess.CheckDB();
+            Assert.AreEqual(true, available);
+        }
+    }
+}
