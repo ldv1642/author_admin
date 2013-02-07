a6e5a89006c5ae01e2ac40d88b6b5397cf78f7de
ldv1642 <ldv1642@live.com>
2013-02-07 14:39:39 +0000

    add_update_test

--- AuthorsDB/DBAccess.cs
+++ AuthorsDB/DBAccess.cs
@@ -16,12 +16,91 @@ namespace AuthorsDB
 
         public static List<Author> GetAuthors()
         {
-            return null;
+            List<Author> authors = new List<Author>();
+   
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                using (SqlCommand comm = conn.CreateCommand())
+                {
+                    comm.CommandType = System.Data.CommandType.StoredProcedure;
+                    comm.CommandText = "GetAuthors";
+
+                    conn.Open();
+                    var reader = comm.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        authors.Add(
+                            new Author()
+                            {
+                                AuthorID=reader["au_id"] as string,
+                                FirstName= reader["au_fname"] as string,
+                                LastName = reader["au_lname"] as string,
+                                Phone = reader["phone"] as string,
+                                Address = reader["address"] as string,
+                                City = reader["city"] as string,
+                                State = reader["State"] as string,
+                                Zip = reader["zip"] as string,
+                                Contract = reader["contract"] as bool?
+                            });
+                    }
+                    conn.Close();
+                }
+            }
+
+            return authors;
         }
 
         public static Author GetAuthor(string id)
         {
-            return null;
+            Author author = new Author();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                using (SqlCommand comm = conn.CreateCommand())
+                {
+                    comm.CommandType = System.Data.CommandType.StoredProcedure;
+                    comm.CommandText = "GetAuthor";
+                    comm.Parameters.AddWithValue("@id", id);
+                    conn.Open();
+                    var reader = comm.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        author.AuthorID = reader["au_id"] as string;
+                        author.FirstName = reader["au_fname"] as string;
+                        author.LastName = reader["au_lname"] as string;
+                        author.Phone = reader["phone"] as string;
+                        author.Address = reader["address"] as string;
+                        author.City = reader["city"] as string;
+                        author.State = reader["State"] as string;
+                        author.Zip = reader["zip"] as string;
+                        author.Contract = reader["contract"] as bool?;
+                    }
+                    conn.Close();
+                }
+            }
+
+            return author;
+        }
+        
+        public static int GetAuthorCount()
+        {
+            int count;
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                using (SqlCommand comm = conn.CreateCommand())
+                {
+                    comm.CommandType = System.Data.CommandType.StoredProcedure;
+                    comm.CommandText = "GetAuthorCount";
+
+                    conn.Open();
+                    var reader = comm.ExecuteReader();
+                    reader.Read();
+                    count = (int)reader["count"];
+                    conn.Close();
+                }
+            }
+
+            return count;
         }
 
         public static int UpdateAuthor(Author author)
@@ -29,9 +108,52 @@ namespace AuthorsDB
             return 0;
         }
 
-        public static int InsertAuthor(Author author)
+        public static string InsertAuthor(Author author)
         {
-            return 0;
+            string id = "";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                using (SqlCommand comm = conn.CreateCommand())
+                {
+                    comm.CommandType = System.Data.CommandType.StoredProcedure;
+                    comm.CommandText = "AddAuthor";
+                    comm.Parameters.AddWithValue("@id", author.AuthorID);
+                    comm.Parameters.AddWithValue("@fname", author.FirstName);
+                    comm.Parameters.AddWithValue("@lname", author.LastName);
+                    comm.Parameters.AddWithValue("@phone", author.Phone);
+                    comm.Parameters.AddWithValue("@contract", author.Contract);
+                    
+                    SqlParameter p = new SqlParameter("@returnID", SqlDbType.VarChar, 11);
+                    p.Direction = ParameterDirection.Output;
+                    comm.Parameters.Add(p);
+                    
+                    conn.Open();
+                    comm.ExecuteNonQuery();
+                    conn.Close();
+                    id = p.Value as string;
+                }
+            }
+            
+            return id;
+        }
+
+        public static int DeleteAuthor(string id)
+        {
+            int result;
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                using (SqlCommand comm = conn.CreateCommand())
+                {
+                    comm.CommandType = System.Data.CommandType.StoredProcedure;
+                    comm.CommandText = "DeleteAuthor";
+                    comm.Parameters.AddWithValue("@id", id);
+                    conn.Open();
+                    result = comm.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+
+            return result;
         }
 
         public static bool CheckDB()
@@ -51,10 +173,5 @@ namespace AuthorsDB
                 }
             }
         }
-    }
-
-    public class Author
-    {
-
     }
 }
--- Test/TestDB.cs
+++ Test/TestDB.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using NUnit;
 using NUnit.Framework;
 using AuthorsDB;
+using Moq;
+using System.Data.SqlClient; //not needed
+using System.Configuration;
 
 namespace Test
 {
@@ -19,11 +22,59 @@ namespace Test
         //delete an author
         //insert an author
 
+        string connString;
+
+        [SetUp]
+        public void Init()
+        {
+            connString = ConfigurationManager.ConnectionStrings["db"].ConnectionString;
+        }
+
         [TestCase]
         public void CanConnectToDB()
         {
             bool available = DBAccess.CheckDB();
             Assert.AreEqual(true, available);
+        }
+
+        [TestCase]
+        public void GetAllAuthors()
+        { 
+            //check count of return list is qual to row count
+            var authors = DBAccess.GetAuthors();
+            int count = DBAccess.GetAuthorCount();
+
+            Assert.AreEqual(authors.Count, count);
+        }
+
+        [TestCase]
+        public void InsertAuthorGetAuthorDeleteAuthor()
+        { 
+            //insert an author and use returned id to get it back
+            string result = DBAccess.InsertAuthor(
+                new Author()
+                {
+                    AuthorID = "822-52-2228",
+                    FirstName = "Test First Name",
+                    LastName = "Test Last Name",
+                    Phone = "051 444-4444",
+                    Address = "Test Address",
+                    City = "Test City",
+                    State = "GB",
+                    Zip = "22222",
+                    Contract = true
+                });
+            //get author
+            var author = DBAccess.GetAuthor(result);
+            //delete author
+            DBAccess.DeleteAuthor(result);
+            Assert.AreEqual(author.AuthorID,result);
+        }
+
+        [TestCase]
+        public void UpdateAuthor()
+        {
+            Assert.Fail("Implement");
         }
     }
 }
